f0b52cb185f1e2c308d197851fd99f675657706d
ckstmznf <ckstmznf@naver.com>
2021-10-02 02:11:50 +0900

    D-Day

--- js/js.js
+++ js/js.js
@@ -5,3 +5,12 @@ for(var i = 0; i < datas.length; i++){
 
     document.querySelectorAll("div.buttons")[0].appendChild(button)
 }
+
+const toDay = new Date()
+const korean = new Date(2021, 10, 8)
+const first = new Date(2021, 10, 5)
+
+const d = korean - toDay
+
+console.log(d);
+document.querySelector(".dDay").innerHTML = `중간고사 D-${first.getDate() - toDay.getDate() === 0 ? 'Day' : first.getDate() - toDay.getDate()}, 국어 시험 D-${korean.getDate() - toDay.getDate() === 0 ? 'Day' : korean.getDate() - toDay.getDate()}`

ab282f9f3d5c3d06997c18a8f0aa3b72c63f2599
ckstmznf <ckstmznf@naver.com>
2021-12-20 17:44:03 +0900

    D-dayy 제거

--- js/js.js
+++ js/js.js
@@ -15,27 +15,3 @@ for(var i = 0; i < datas.length; i++){
 // button.setAttribute("onclick", `location.href = './jisi.html'`)
 // document.querySelectorAll("div.buttons")[0].appendChild(button)
 
-
-const toDay = new Date()
-const korean = new Date(2021, 10, 8)
-const first = new Date(2021, 10, 5)
-
-const d = korean - toDay
-
-console.log(d);
-
-let dDay = ""
-if(first.getDate() - toDay.getDate() === 0){
-    dDay += `중간고사 D-Day`
-}   else if(first.getDate() - toDay.getDate() > 0){
-    dDay += `중간고사 D-${first.getDate() - toDay.getDate()},`
-}
-
-if(korean.getDate() - toDay.getDate() === 0){
-    dDay += `국어 시험 D-Day`
-}   else if(korean.getDate() - toDay.getDate() > 0){
-    dDay += `국어 시험 D-${korean.getDate() - toDay.getDate()}`
-}
-
-
-document.querySelector(".dDay").innerHTML = dDay
